14256f51a3a1b8b2c86be112b9f3977af97354a7
zhangzhenli <zhangzhenli@live.com>
2016-08-19 13:37:53 +0800

    oauth token implement cas ticket

--- cas-server-support-oauth-core/src/main/java/org/apereo/cas/ticket/accesstoken/AccessTokenImpl.java
+++ cas-server-support-oauth-core/src/main/java/org/apereo/cas/ticket/accesstoken/AccessTokenImpl.java
@@ -1,12 +1,16 @@
 package org.apereo.cas.ticket.accesstoken;
 
 import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.Service;
-import org.apereo.cas.ticket.code.OAuthCodeImpl;
 import org.apereo.cas.ticket.ExpirationPolicy;
+import org.apereo.cas.ticket.TicketGrantingTicket;
+import org.apereo.cas.ticket.code.OAuthCodeImpl;
+import org.apereo.cas.ticket.refreshtoken.RefreshToken;
+import org.apereo.cas.ticket.refreshtoken.RefreshTokenImpl;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.*;
 
 /**
  * An OAuth access token implementation.
@@ -15,10 +19,19 @@ import javax.persistence.Entity;
  * @since 5.0.0
  */
 @Entity
+@Table(name="OAUTH_ACCESS_TOKENS")
 @DiscriminatorValue(AccessToken.PREFIX)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class AccessTokenImpl extends OAuthCodeImpl implements AccessToken {
 
-    private static final long serialVersionUID = 2339545346159721563L;
+    private static final long serialVersionUID = -8287917546624594512L;
+
+    @Lob
+    @Column(name="SCOPES")
+    private Set<String> scope;
+
+    @ManyToOne(targetEntity = RefreshTokenImpl.class)
+    private TicketGrantingTicket ticketGrantingTicket;
 
     /**
      * Instantiates a new OAuth access token.
@@ -39,5 +52,56 @@ public class AccessTokenImpl extends OAuthCodeImpl implements AccessToken {
     public AccessTokenImpl(final String id, final Service service, final Authentication authentication,
                            final ExpirationPolicy expirationPolicy) {
         super(id, service, authentication, expirationPolicy);
+        scope = null;
+    }
+
+    /**
+     * Constructs a new limited scope access token with unique id for a service and authentication.
+     *
+     * @param id               the unique identifier for the ticket.
+     * @param service          the service this ticket is for.
+     * @param authentication   the authentication.
+     * @param expirationPolicy the expiration policy.
+     * @param scopes           limited scope
+     * @throws IllegalArgumentException if the service or authentication are null.
+     */
+    public AccessTokenImpl(final String id, final Service service, final Authentication authentication,
+                           final ExpirationPolicy expirationPolicy, List<String> scopes) {
+        super(id, service, authentication, expirationPolicy);
+        scope = new HashSet<>(scopes);
+    }
+
+    @Override
+    public boolean isValidFor(Service serviceToValidate) {
+        update();
+        if (scope == null)
+            return true;
+        return this.scope.contains(serviceToValidate.getId());
+    }
+
+    @Override
+    public TicketGrantingTicket getGrantingTicket() {
+        return this.ticketGrantingTicket;
+    }
+
+    public void setRefreshToken(RefreshToken refreshToken) {
+        this.ticketGrantingTicket = refreshToken;
+    }
+
+    @Override
+    public void setPrincipal(Principal principal) {
+
+    }
+
+    @Override
+    public boolean matches(Service service) {
+        return this.getId().matches(service.getId());
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("scope", scope);
+        return stringObjectHashMap;
     }
 }
